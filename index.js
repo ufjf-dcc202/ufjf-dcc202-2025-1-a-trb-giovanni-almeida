b7f78bb403353fcfb3b2b3987add388c48272711
giovannikvothe <reheal1504@gmail.com>
2025-07-27 14:15:52 -0300

    Adicionando parte funcional do plantio

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const jardim = document.getElementById("jardim");
 
+let semente = null;
+
 for (let i = 0; i < 12; i++) {
     let linha = document.createElement("tr");
     for (let j = 0; j < 12; j++) {
         let espaco = document.createElement("td");
-        linha.appendChild(espaco)
+        espaco.etapa = 0;
+        linha.appendChild(espaco);
     }
-    jardim.appendChild(linha)
+    jardim.appendChild(linha);
 }
+
+jardim.addEventListener("mousedown", (e) => {
+    let espaco = e.target;
+    
+    if (espaco.classList.contains("pedras") || espaco.classList.contains("mato")) {
+        espaco.classList = "";
+        espaco.classList.add("plantavel");
+    }
+
+    if (semente && espaco.classList.contains("plantavel")) {
+        espaco.classList.remove("plantavel");
+        espaco.classList.add(semente);
+    }
+})
