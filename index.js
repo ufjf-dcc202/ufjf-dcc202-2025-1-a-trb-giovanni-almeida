7706d9e39fa3f7608c7a249eed12d3140b946ac7
giovannikvothe <reheal1504@gmail.com>
2025-07-27 09:12:23 -0300

    Criação do jardim de plantio

---
+++ index.js
@@ -0,0 +1,8 @@
+const jardim = document.getElementById("jardim");
+
+for (let i = 0; i < 12; i++) {
+    let linha = document.createElement("tr");
+    for (let j = 0; j < 12; j++) {
+        let espaco = document.createElement("td");
+    }
+}
