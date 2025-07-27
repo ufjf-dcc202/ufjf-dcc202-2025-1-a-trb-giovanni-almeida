7718785dfb782fab5900ce69e5c8f1d380d9429c
giovannikvothe <reheal1504@gmail.com>
2025-07-27 16:57:20 -0300

    Adicionado os tipos de planta e mais partes funcionais do plantio

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const jardim = document.getElementById("jardim");
 
+
 document.getElementById('semente1').addEventListener('click', () => semente = 'semente1');
 document.getElementById('semente2').addEventListener('click', () => semente = 'semente2');
 document.getElementById('semente3').addEventListener('click', () => semente = 'semente3');
-
 let semente = null;
+
 
 for (let i = 0; i < 12; i++) {
     let linha = document.createElement("tr");
@@ -17,16 +18,77 @@ for (let i = 0; i < 12; i++) {
     jardim.appendChild(linha);
 }
 
+
 jardim.addEventListener("mousedown", (e) => {
     let espaco = e.target;
-    if (espaco.classList.contains("pedras") || espaco.classList.contains("mato")) {
+    if (espaco.classList.contains("pedra") || espaco.classList.contains("mato")) {
         espaco.classList = "";
         espaco.classList.add("plantavel");
     }
-    
+
     if (semente && espaco.classList.contains("plantavel")) {
         console.log(semente)
         espaco.classList.remove("plantavel");
+        espaco.classList.add("plantado");
         espaco.classList.add(semente);
     }
+
+    document.getElementById('regar').addEventListener('click', () => {
+        document.querySelectorAll('#jardim td.plantado').forEach(espaco => {
+            espaco.classList.add('irrigada');
+        });
+    });
+
+    document.getElementById('tempo').addEventListener('click', () => {
+        document.querySelectorAll('#jardim td.plantado').forEach(espaco => {
+            if (espaco.classList.contains("irrigada")) {
+                // Planta 1
+                if (espaco.classList.contains("semente1")) {
+                    espaco.classList.remove("semente1");
+                    espaco.classList.add("semente1_2");
+                    espaco.classList.remove("irrigada");
+                    return;
+                }
+
+                if (espaco.classList.contains("semente1_2")) {
+                    espaco.classList.remove("semente1_2");
+                    espaco.classList.add("semente1_3");
+                    espaco.classList.remove("irrigada");
+                    return;
+                }
+
+
+                // Planta 2
+                if (espaco.classList.contains("semente2")) {
+                    espaco.classList.remove("semente2");
+                    espaco.classList.add("semente2_2");
+                    espaco.classList.remove("irrigada");
+                    return;
+                }
+
+                if (espaco.classList.contains("semente2_2")) {
+                    espaco.classList.remove("semente2_2");
+                    espaco.classList.add("semente2_3");
+                    espaco.classList.remove("irrigada");
+                    return;
+                }
+
+
+                // Planta 3
+                if (espaco.classList.contains("semente3")) {
+                    espaco.classList.remove("semente3");
+                    espaco.classList.add("semente3_2");
+                    espaco.classList.remove("irrigada");
+                    return;
+                }
+
+                if (espaco.classList.contains("semente3_2")) {
+                    espaco.classList.remove("semente3_2");
+                    espaco.classList.add("semente3_3");
+                    espaco.classList.remove("irrigada");
+                    return;
+                }
+            }
+        });
+    });
 })
