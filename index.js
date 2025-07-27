1b3c24d0b1deabe9544ee0458592c341da7be63e
giovannikvothe <reheal1504@gmail.com>
2025-07-27 14:59:56 -0300

    Adicionado algumas artes e mais partes funcionais do plantio

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const jardim = document.getElementById("jardim");
 
+document.getElementById('semente1').addEventListener('click', () => semente = 'semente1');
+document.getElementById('semente2').addEventListener('click', () => semente = 'semente2');
+document.getElementById('semente3').addEventListener('click', () => semente = 'semente3');
+
 let semente = null;
 
 for (let i = 0; i < 12; i++) {
@@ -7,6 +11,7 @@ for (let i = 0; i < 12; i++) {
     for (let j = 0; j < 12; j++) {
         let espaco = document.createElement("td");
         espaco.etapa = 0;
+        espaco.classList.add("plantavel")
         linha.appendChild(espaco);
     }
     jardim.appendChild(linha);
@@ -14,13 +19,13 @@ for (let i = 0; i < 12; i++) {
 
 jardim.addEventListener("mousedown", (e) => {
     let espaco = e.target;
-    
     if (espaco.classList.contains("pedras") || espaco.classList.contains("mato")) {
         espaco.classList = "";
         espaco.classList.add("plantavel");
     }
-
+    
     if (semente && espaco.classList.contains("plantavel")) {
+        console.log(semente)
         espaco.classList.remove("plantavel");
         espaco.classList.add(semente);
     }
