a435b4e6a160d539b7655b63c3c999bbebb250b4
giovannikvothe <reheal1504@gmail.com>
2025-07-27 09:20:45 -0300

    Melhoria no jardim de plantio

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ for (let i = 0; i < 12; i++) {
     let linha = document.createElement("tr");
     for (let j = 0; j < 12; j++) {
         let espaco = document.createElement("td");
+        linha.appendChild(espaco)
     }
+    jardim.appendChild(linha)
 }
